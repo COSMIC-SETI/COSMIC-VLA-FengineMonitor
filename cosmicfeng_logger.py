a2a3a09d667f8d0043b0c6c712180af77da971fa
talonmyburgh <talon.myburgh@mydon.co.za>
2022-10-06 10:20:14 +0000

    +* arg parsing and dts_parity_errs

--- cosmicfeng_logger.py
+++ cosmicfeng_logger.py
@@ -2,6 +2,10 @@ from cosmic.fengines import ant_remotefeng_map
 from cosmic.hashpipe_aux import redis_obj
 import time
 import logging
+import os
+import argparse
+
+LOGFILENAME = "/home/cosmic/logs/F_Engines.log"
 
 logger = logging.getLogger('cosmicfeng_logger')
 logger.setLevel(logging.INFO)
@@ -9,7 +13,7 @@ logger.setLevel(logging.INFO)
 # create console handler and set level to debug
 ch = logging.StreamHandler()
 ch.setLevel(logging.INFO)
-fh = logging.FileHandler("/home/cosmic/logs/F_Engines.log")
+fh = logging.FileHandler(LOGFILENAME)
 fh.setLevel(logging.INFO)
 
 # create formatter
@@ -29,7 +33,7 @@ class FEngineLogger:
     desired status'
     """
 
-    def __init__(self, redis_obj, polling_rate=30):
+    def __init__(self, redis_obj, polling_rate):
         self.redis_obj = redis_obj
         self.polling_rate = polling_rate
         self.ant_feng_map = ant_remotefeng_map.get_antennaFengineDict(
@@ -42,11 +46,28 @@ class FEngineLogger:
         while True:
             for ant, feng in self.ant_feng_map.items():
                 dts_status = feng.dts.get_status()
+                dts_parity_errs = feng.dts.get_parity_errs()
                 delay_status = feng.delay.get_status()
                 lo_status = feng.lo.get_status()
-                logger.info(f"""******STATUS RECORDING FOR ANTENNA {ant}******:\nDTS STATUS:\n {dts_status}\nDELAY STATUS:\n {delay_status}\nLO STATUS:\n {lo_status}\n""")
+                logger.info(f"""******STATUS RECORDING FOR ANTENNA {ant}******:\nDTS STATUS:\n{dts_status}\nDTS PARITY ERRS:\n{dts_parity_errs}\nDELAY STATUS:\n{delay_status}\nLO STATUS:\n{lo_status}\n""")
             time.sleep(self.polling_rate)
 
 if __name__ == "__main__":
-    feng_logger = FEngineLogger(redis_obj)
+    parser = argparse.ArgumentParser(
+    description=("Set up the FEngine logger.")
+    )
+    parser.add_argument(
+    "-p","--polling_rate", type=int, help="FEngine polling rate in seconds.", default=30
+    )
+    parser.add_argument(
+    "-c", "--clean", action="store_true",help="Delete the existing log file and start afresh.",
+    )
+    args = parser.parse_args()
+    if os.path.exists(LOGFILENAME) and args.clean:
+        print("Removing previous log file...")
+        os.remove(LOGFILENAME)
+    else:
+        print("Nothing to clean, continuing...")
+
+    feng_logger = FEngineLogger(redis_obj, polling_rate = args.polling_rate)
     feng_logger.run()
